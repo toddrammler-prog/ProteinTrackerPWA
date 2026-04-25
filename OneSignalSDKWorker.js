c5019029991f3b4b33d3e994161376f44f681ae0
toddrammler-prog <todd.rammler@gmail.com>
2026-04-25 17:46:19 -0400

    Add files via upload

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,4 +1,4 @@
-const CACHE = 'protein-tracker-v12';
+const CACHE = 'protein-tracker-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
